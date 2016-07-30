e92ecf6d8db7b77ac5c02d69e50a1b0a53856b3c
Bart Jacobs <developer@bartjacobs.com>
2016-07-30 11:21:51 +0200

    Create, size, and position form

--- Auto Layout in Code/ViewController.swift
+++ Auto Layout in Code/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
 
     private func setupView() {
         setupContainerView()
+
+        setupForm()
     }
 
     private func setupContainerView() {
@@ -121,6 +123,87 @@ class ViewController: UIViewController {
         
         // Add Constraint
         view.addConstraint(constraintTrailing)
+    }
+
+    private func setupForm() {
+        // Initialize Labels
+        let labelFirstName = UILabel()
+        labelFirstName.text = "First Name"
+        labelFirstName.textAlignment = .Right
+        labelFirstName.setContentHuggingPriority(251.0, forAxis: .Horizontal)
+
+        let labelLastName = UILabel()
+        labelLastName.text = "Last Name"
+        labelLastName.textAlignment = .Right
+        labelLastName.setContentHuggingPriority(251.0, forAxis: .Horizontal)
+
+        let labelEmail = UILabel()
+        labelEmail.text = "Email"
+        labelEmail.textAlignment = .Right
+        labelEmail.setContentHuggingPriority(251.0, forAxis: .Horizontal)
+
+        // Initialize Text Fields
+        let textFieldFirstName = UITextField()
+        textFieldFirstName.borderStyle = .RoundedRect
+
+        let textFieldLastName = UITextField()
+        textFieldLastName.borderStyle = .RoundedRect
+
+        let textFieldEmail = UITextField()
+        textFieldEmail.borderStyle = .RoundedRect
+
+        // Initialize Submit Button
+        let submitButton = UIButton()
+        submitButton.setTitle("Submit", forState: .Normal)
+        submitButton.setTitleColor(UIColor(red: 0.086, green: 0.494, blue: 0.984, alpha: 1.0), forState: .Normal)
+
+        let metrics = [
+            "heightLabel" : 30.0,
+            "heightTextField" : 30.0,
+            "heightSubmitButton" : 30.0
+        ]
+
+        let views = [
+            "labelFirstName" : labelFirstName,
+            "textFieldFirstName" : textFieldFirstName,
+            "labelLastName" : labelLastName,
+            "textFieldLastName" : textFieldLastName,
+            "labelEmail" : labelEmail,
+            "textFieldEmail" : textFieldEmail,
+            "submitButton" : submitButton
+        ]
+
+        for (_, subview) in views {
+            // Configure Subview
+            subview.translatesAutoresizingMaskIntoConstraints = false
+
+            // Add to Superview
+            containerView.addSubview(subview)
+        }
+
+        let formatHorizontalFirstName = "H:|-8-[labelFirstName]-8-[textFieldFirstName]-8-|"
+        let formatHorizontalLastName = "H:|-8-[labelLastName]-8-[textFieldLastName]-8-|"
+        let formatHorizontalEmail = "H:|-8-[labelEmail]-8-[textFieldEmail]-8-|"
+        let formatHorizontalSubmitButton = "H:|-8-[submitButton]-8-|"
+
+        let formatVerticalLabels = "V:|-8-[labelFirstName(==heightLabel)]-8-[labelLastName(==heightLabel)]-8-[labelEmail(==heightLabel)]-8-[submitButton(==heightSubmitButton)]"
+        let formatVerticalTextFields = "V:|-8-[textFieldFirstName(==heightTextField)]-8-[textFieldLastName(==heightTextField)]-8-[textFieldEmail(==heightTextField)]"
+
+        let constraintsHorizontalFirstName = NSLayoutConstraint.constraintsWithVisualFormat(formatHorizontalFirstName, options: [], metrics: nil, views: views)
+        let constraintsHorizontalLastName = NSLayoutConstraint.constraintsWithVisualFormat(formatHorizontalLastName, options: [], metrics: nil, views: views)
+        let constraintsHorizontalEmail = NSLayoutConstraint.constraintsWithVisualFormat(formatHorizontalEmail, options: [], metrics: nil, views: views)
+        let constraintsHorizontalSubmitButton = NSLayoutConstraint.constraintsWithVisualFormat(formatHorizontalSubmitButton, options: [], metrics: nil, views: views)
+
+        let constraintsVerticalLabels = NSLayoutConstraint.constraintsWithVisualFormat(formatVerticalLabels, options: [], metrics: metrics, views: views)
+        let constraintsVerticalTextFields = NSLayoutConstraint.constraintsWithVisualFormat(formatVerticalTextFields, options: [.AlignAllLeading], metrics: metrics, views: views)
+
+        containerView.addConstraints(constraintsHorizontalFirstName)
+        containerView.addConstraints(constraintsHorizontalLastName)
+        containerView.addConstraints(constraintsHorizontalEmail)
+        containerView.addConstraints(constraintsHorizontalSubmitButton)
+        
+        containerView.addConstraints(constraintsVerticalLabels)
+        containerView.addConstraints(constraintsVerticalTextFields)
     }
 
 }
